f96baffbcf7d4db4a9d48bb33398e93488014be7
wljince007 <liangwang007@gmail.com>
2023-07-14 14:48:54 +0800

    1.0.0. code refactoring.  readme.

--- src/services/sftpTab.service.ts
+++ src/services/sftpTab.service.ts
@@ -35,6 +35,11 @@ export class SftpTabService {
       }
     });
     this.logger.info('init succ!');
+    this.config.ready$.toPromise().then(() => {
+      this.tryAddSftpProfilesTemplateToConfigService().then(() => {
+        this.logger.info('call tryAddSftpProfilesTemplateToConfigService after config.ready');
+      })
+    })
   }
 
   findFirstSshProfile(token: any):   Profile | null {
@@ -58,47 +63,40 @@ export class SftpTabService {
     return null
   }
 
-
-  async findSftpProfile(profileName: string): Promise<PartialProfile<Profile>|null> {
-    let tmpprofile = (await this.profilesService.getProfiles()).find(x => x.name === profileName)
-    if (!tmpprofile) {
-      // Attempt to add a default sftp profile when not found
-      this.logger.warn(`Requested profile ${profileName} not found, try adding ...`, profileName);
-      this.sftpTabConfigProvider.defaults.profiles.forEach(profile => {
-        let find_in_config : boolean = false
-        this.config.store.profiles.forEach(profile1 => {
-          if (profile1?.name == profile.name){
-            find_in_config = true
-          }
-        });
-        if (!find_in_config) {
-          this.config.store.profiles = [...this.config.store.profiles, deepClone(profile)]
+  // add sftp profile template to config service and save to config file
+  async tryAddSftpProfilesTemplateToConfigService(): Promise<void> {
+    let modifiedConfig :boolean = false
+    this.sftpTabConfigProvider.defaults.profiles.forEach(sftpProfile => {
+      let find_in_config : boolean = false
+      this.config.store.profiles.forEach(profile => {
+        if (profile?.name == sftpProfile.name){
+          find_in_config = true
         }
       });
+      if (!find_in_config) {
+        this.config.store.profiles = [...this.config.store.profiles, deepClone(sftpProfile)]
+        modifiedConfig = true
+      }
+    });
+    if (modifiedConfig) {
       await this.config.save()
       await this.config.load()
-
+  
       // If the settings tab is already open, close and reopen the settings tab on the same tabs index.
       const settingsTabOld = this.app.tabs.find(tab => tab instanceof SettingsTabComponent)
       if (settingsTabOld) {
           const settingsTabOldIdx = this.app.tabs.indexOf(settingsTabOld)
           this.app.closeTab(settingsTabOld)
-
+  
           const settingsTab = this.tabsService.create({ type: SettingsTabComponent })
           this.app.addTabRaw(settingsTab,settingsTabOldIdx)
           this.app.selectTab(settingsTab)
       }
     }
-
-    tmpprofile = (await this.profilesService.getProfiles()).find(x => x.name === profileName)
-    return tmpprofile
   }
 
-  async openSftpImpl (tab: BaseTabComponent|null): Promise<BaseTabComponent|null>{
-    let ans :BaseTabComponent|null = null
-    if (tab == null) {
-      tab = this.app.activeTab
-    }
+
+  async findSftpProfileTemplate(): Promise<PartialProfile<Profile>|null> {
     let profileName =""
     if (this.hostApp.platform === Platform.macOS) {
         profileName = "ssh2sftp_mac_template"
@@ -110,19 +108,35 @@ export class SftpTabService {
         return null
     }
 
-    let tmpprofile = await this.findSftpProfile(profileName)
-    if (!tmpprofile) {
+    let sftpProfileTemplate = (await this.profilesService.getProfiles()).find(x => x.name === profileName)
+    if (!sftpProfileTemplate) {
+      // Attempt to add a default sftp profile when not found
+      this.logger.warn(`Requested profile ${profileName} not found, try adding ...`, profileName);
+      await this.tryAddSftpProfilesTemplateToConfigService()
+      sftpProfileTemplate = (await this.profilesService.getProfiles()).find(x => x.name === profileName)
+    }
+    return sftpProfileTemplate
+  }
+
+  async openSftpImpl (tab: BaseTabComponent|null): Promise<BaseTabComponent|null>{
+    let ans :BaseTabComponent|null = null
+    if (tab == null) {
+      tab = this.app.activeTab
+    }
+
+    let sftpProfileTemplate = await this.findSftpProfileTemplate()
+    if (!sftpProfileTemplate) {
         // Report an error if it cannot be found the second time
-        this.logger.warn(`Requested profile ${tmpprofile} not found`, tmpprofile);
+        this.logger.warn(`Requested profile ${sftpProfileTemplate} not found`, sftpProfileTemplate);
         return null
     }else {
-        let profile = deepClone(tmpprofile)
+        let sftpProfile = deepClone(sftpProfileTemplate)
         const token = await tab.getRecoveryToken()
         let sshprofile : Profile | null =this.findFirstSshProfile(token)
         if (sshprofile != null) {
-          profile.name =  "sftp_" + sshprofile?.name
-          if(profile?.options && sshprofile?.options){
-              let args = profile.options["args"]
+          sftpProfile.name =  "sftp_" + sshprofile?.name
+          if(sftpProfile?.options && sshprofile?.options){
+              let args = sftpProfile.options["args"]
               if (args as Array<string>) {
                   args.push("-P")
                   const port = sshprofile.options.port as number
@@ -130,14 +144,16 @@ export class SftpTabService {
                   args.push(sshprofile.options.user as string + "@" + sshprofile.options.host as string)
               }
           }
-          let params = await this.profilesService.newTabParametersForProfile(profile)
+          let params = await this.profilesService.newTabParametersForProfile(sftpProfile)
           if (params) {
               const sftptab = this.tabsService.create(params)
-              this.app.addTabRaw(sftptab,this.app.tabs.indexOf(tab)+1)
-              this.app.selectTab(sftptab)
-              ans = sftptab
+              if (sftptab) {
+                  this.app.addTabRaw(sftptab,this.app.tabs.indexOf(tab)+1)
+                  this.app.selectTab(sftptab)
+                  ans = sftptab
+                  this.logger.info('open sftp tab for ssh profile succ');
+              }
           }
-          this.logger.info('open sftp tab for ssh profile succ');
           return ans
         }else {
             this.logger.warn('open sftp tab just for ssh profile connection。 can not find ssh profile! can not open sftp tab!!!');
